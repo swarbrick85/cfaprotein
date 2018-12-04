845e7efa351308c8c892f49f8d6722d70d41819c
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-04 19:52:33 +0000

    Item class created

---
+++ lib/item.rb
@@ -0,0 +1,17 @@
+require_relative './environment.rb'
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Item 
+  
+  attr_accessor :name, :link, :protein, :fat, :complex_carbs, :simple_carbs, :total_carbs, :calories, :pct_cal_by_protein, :pct_cal_by_fat, :pct_cal_by_sc, :pct_cal_by_cc, :pct_cal_by_tc, :cal_per_protein
+  
+  def initialize(name, link)
+    @name = name 
+    @link = link
+  end   
+  
+  
+end 
