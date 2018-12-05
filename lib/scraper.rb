792341cf2a5c2aea7a48a1a8b99f5b59383ce127
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-05 21:18:03 +0000

    more

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,16 +16,19 @@ class Scraper
   def create_breakfast_menu
     a = 0 
     15.times{
-    x = Item.new 
-    Item.breakfast_all << x 
-    z = front_page.css("td")[3 + (11 * a)].text 
-    z[0...11] = ""
-    x.name = z 
-    x.calories = front_page.css("td")[4 + (11 * a)].text
-    x.fat = front_page.css("td")[5 + (11 * a)].text
-    x.total_carbs = front_page.css("td")[10 + (11 * a)].text
-    x.sugar
-    a += 1
+      if front_page.css("td")[13 + (11 * a)].text !== "0"
+         x = Item.new 
+         Item.breakfast_all << x 
+         z = front_page.css("td")[3 + (11 * a)].text 
+         z[0...11] = ""
+         x.name = z 
+         x.calories = front_page.css("td")[4 + (11 * a)].text
+         x.fat = front_page.css("td")[5 + (11 * a)].text
+         x.total_carbs = front_page.css("td")[10 + (11 * a)].text
+         x.sugar = front_page.css("td")[12 + (11 * a)].text
+         x.protein = front_page.css("td")[13 + (11 * a)].text
+         a += 1
+       end 
     }
      
   end 
