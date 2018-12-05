d3a989bec18894a24dc510d7eb6b8a167fd57d88
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-05 23:47:29 +0000

    more

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,8 @@ require 'pry'
 
 class Scraper 
   
+  include Cfaprotein
+  
   def front_page 
     doc = Nokogiri::HTML(open("https://www.nutritionix.com/chick-fil-a/menu/premium/"))
     doc 
@@ -27,7 +29,7 @@ class Scraper
          x.total_carbs = front_page.css("td")[10 + (11 * a)].text
          x.sugar = front_page.css("td")[12 + (11 * a)].text
          x.protein = front_page.css("td")[13 + (11 * a)].text
-         include Cfaprotein 
+         calculate 
        end 
        
       a += 1
