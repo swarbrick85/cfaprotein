9354fd5f38a656a8ce2be7976b6e7c914258fec5
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-05 23:02:44 +0000

    more

--- lib/cfaprotein.rb
+++ lib/cfaprotein.rb
@@ -2,20 +2,20 @@ require "cfaprotein/version"
 
 module Cfaprotein
   class Error < StandardError; end
-  def create_breakfast_menu
+  def menu
     a = 0 
-    15.times{
-      if (front_page.css("td")[13 + (11 * a)].text != "0")
+    b.times{
+      if (front_page.css("td")[c + 10 + (11 * a)].text != "0")
          x = Item.new 
          Item.breakfast_all << x 
-         z = front_page.css("td")[3 + (11 * a)].text 
+         z = front_page.css("td")[c + (11 * a)].text 
          z[0...11] = ""
          x.name = z 
-         x.calories = front_page.css("td")[4 + (11 * a)].text
-         x.fat = front_page.css("td")[5 + (11 * a)].text
-         x.total_carbs = front_page.css("td")[10 + (11 * a)].text
-         x.sugar = front_page.css("td")[12 + (11 * a)].text
-         x.protein = front_page.css("td")[13 + (11 * a)].text
+         x.calories = front_page.css("td")[c + 1 + (11 * a)].text
+         x.fat = front_page.css("td")[c + 2 + (11 * a)].text
+         x.total_carbs = front_page.css("td")[c + 7 + (11 * a)].text
+         x.sugar = front_page.css("td")[12 + (c + 9 * a)].text
+         x.protein = front_page.css("td")[c + 10 + (11 * a)].text
        end 
        
       a += 1
