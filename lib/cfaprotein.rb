968b11ee93dc662a798d83a86de057b19b03dbe8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-06 08:28:20 +0000

    more

--- lib/cfaprotein.rb
+++ lib/cfaprotein.rb
@@ -43,16 +43,5 @@ module Cfaprotein
     all_class_variable
   end
   
-  def calculate 
-    
-    
-         x.complex_carbs = (x.total_carbs - x.sugar).to_f.round(1)
-         x.pct_cal_by_protein = (((x.protein * 4)/x.calories) * 100).round(1)
-         x.pct_cal_by_fat = (((x.fat * 9)/x.calories) * 100).round(1)
-         x.pct_cal_by_sc = (((x.sugar * 4)/x.calories) * 100).round(1)
-         x.pct_cal_by_cc = (((x.complex_carbs * 4)/x.calories) * 100).round(1)
-         x.pct_cal_by_tc = (((x.total_carbs * 4)/x.calories) * 100).round(1)
-         x.cal_per_protein = (x.calories/x.protein).round(1)
-    
-  end 
+  
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,55 +34,11 @@ class Scraper
   end 
   
   def create_sides_menu
-    
-    b = 10 
-    c = 402
-    
-    a = 0 
-     b.times{
-      if (front_page.css("td")[c + 10 + (@row_width * a)].text != "0")
-         x = Item.new 
-         Item.sides_all << x 
-         z = front_page.css("td")[c + (@row_width * a)].text 
-         z[0...11] = ""
-         x.name = z 
-         x.calories = front_page.css("td")[c + 1 + (@row_width * a)].text
-         x.fat = front_page.css("td")[c + 2 + (@row_width * a)].text
-         x.total_carbs = front_page.css("td")[c + 7 + (@row_width * a)].text
-         x.sugar = front_page.css("td")[c + 9 + (@row_width * a)].text
-         x.protein = front_page.css("td")[c + 10 + (@row_width * a)].text
-       end 
-       
-       a += 1
-    }
-     
+    create_menu(10, 402, Item.sides_all)
   end 
   
   def create_treats_menu
-   
-    b = 18
-    c = 513
-   
-    a = 0 
-     b.times{
-      if (front_page.css("td")[c + 10 + (@row_width * a)].text != "0")
-         x = Item.new 
-         Item.treats_all << x 
-         z = front_page.css("td")[c + (@row_width * a)].text 
-         z[0...11] = ""
-         x.name = z 
-         x.calories = front_page.css("td")[c + 1 + (@row_width * a)].text
-         x.fat = front_page.css("td")[c + 2 + (@row_width * a)].text
-         x.total_carbs = front_page.css("td")[c + 7 + (@row_width * a)].text
-         x.sugar = front_page.css("td")[c + 9 + (@row_width * a)].text
-         x.protein = front_page.css("td")[c + 10 + (@row_width * a)].text
-       end 
-       
-       a += 1
-    }
-       
-
-     
+    create_menu(18, 513, Item.treats_all)
   end 
   
   def create_drinks_menu
