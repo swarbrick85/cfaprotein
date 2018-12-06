c427bbb08429d05e35b57902bcc0b1717a80fb1e
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-06 09:32:00 +0000

    more

--- lib/clifunctions.rb
+++ lib/clifunctions.rb
@@ -1,5 +1,11 @@
 Module clifunctions 
-  display_menu(Item)
+  display_menu(item_all_class_variable)
+    a = 1 
+    item_all_class_variable.each do |item|
+      puts "#{a}. #{item.name}"
+      a += 1 
+    end 
+  end 
 
 
 
