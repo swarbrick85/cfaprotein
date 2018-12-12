cb10a9137a32151644e1d60cd9aaa64542211bf9
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-12 17:26:53 +0000

    more CLI

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -64,28 +64,24 @@ class Scraper
     a = 0 
      b.times{
       if (front_page.css("td")[c + 10 + (@row_width * a)].text != "0")
-         x = Item.new 
-         item_all_class_variable << x 
-         item_all_original_class_variable << x
-         Item.all << x 
-         Item.all_orig << x 
          z = front_page.css("td")[c + (@row_width * a)].text 
          z[0...11] = ""
-         x.name = z 
-         x.calories = front_page.css("td")[c + 1 + (@row_width * a)].text.to_f.round(1)
-         x.fat = front_page.css("td")[c + 2 + (@row_width * a)].text.to_f.round(1)
-         x.total_carbs = front_page.css("td")[c + 7 + (@row_width * a)].text.to_f.round(1)
-         x.sugar = front_page.css("td")[c + 9 + (@row_width * a)].text.to_f.round(1)
-         x.protein = front_page.css("td")[c + 10 + (@row_width * a)].text.to_f.round(1)
+         
+         name = z 
+         calories = front_page.css("td")[c + 1 + (@row_width * a)].text.to_f.round(1)
+         fat = front_page.css("td")[c + 2 + (@row_width * a)].text.to_f.round(1)
+         total_carbs = front_page.css("td")[c + 7 + (@row_width * a)].text.to_f.round(1)
+         sugar = front_page.css("td")[c + 9 + (@row_width * a)].text.to_f.round(1)
+         protein = front_page.css("td")[c + 10 + (@row_width * a)].text.to_f.round(1)
         
-         x.complex_carbs = (x.total_carbs - x.sugar).to_f.round(1)
+         complex_carbs = (x.total_carbs - x.sugar).to_f.round(1)
         
-         x.pct_cal_by_protein = (((x.protein * 4)/x.calories) * 100).round(1)
-         x.pct_cal_by_fat = (((x.fat * 9)/x.calories) * 100).round(1)
-         x.pct_cal_by_sc = (((x.sugar * 4)/x.calories) * 100).round(1)
-         x.pct_cal_by_cc = (((x.complex_carbs * 4)/x.calories) * 100).round(1)
-         x.pct_cal_by_tc = (((x.total_carbs * 4)/x.calories) * 100).round(1)
-         x.cal_per_protein = (x.calories/x.protein).round(1)  
+         pct_cal_by_protein = (((x.protein * 4)/x.calories) * 100).round(1)
+         pct_cal_by_fat = (((x.fat * 9)/x.calories) * 100).round(1)
+         pct_cal_by_sc = (((x.sugar * 4)/x.calories) * 100).round(1)
+         pct_cal_by_cc = (((x.complex_carbs * 4)/x.calories) * 100).round(1)
+         pct_cal_by_tc = (((x.total_carbs * 4)/x.calories) * 100).round(1)
+         cal_per_protein = (x.calories/x.protein).round(1)  
        end 
        
       a += 1
