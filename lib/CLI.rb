afaa638ca0ba773f074d2b16022633e66450a258
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-12 19:00:57 +0000

    more CLI

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -293,6 +293,26 @@ class Cli
    
   end 
   
+  def individual_item_path_post_menu_select(item_all_orig_class_var)
+    display_menu(item_all_orig_class_var)
+      y = gets.strip.to_f  
+        until (y.to_f == y.to_i && (y > 0 && y < 18)) || y == 99
+          puts "I'm sorry, that's not an option here."
+          display_menu(Item.entrees_all_orig)
+          y = gets.strip.to_f
+        end 
+        if y == 99 
+          start 
+        end 
+        a = 1 
+          17.times {
+            if y == a 
+              display_item(Item.entrees_all_orig[a-1])
+            end 
+            a += 1
+          }
+  end 
+  
   def individual_item_path 
     list_menus
     puts "99. Start the app over"
